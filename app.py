e85cbe65d2953c0e191a7ef0df9c20799762338b
Christopher Martin <christopher.blake.martin@cern.ch>
2019-06-14 08:03:00 -0400

    bootstrap and form

--- app.py
+++ app.py
@@ -1,9 +1,54 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def hello():
-    return "Insight web app created by chmartin."
+# This is modified from: https://gist.github.com/ericbarnhill/251df20105991674c701d33d65437a50 
+# and https://github.com/mbr/flask-bootstrap/blob/master/sample_application/ 
+
+from flask import Flask, request, render_template, flash
+from flask_bootstrap import Bootstrap
+from flask_appconfig import AppConfig
+from flask_wtf import Form, RecaptchaField
+from flask_wtf.file import FileField
+from wtforms import TextField, HiddenField, ValidationError, RadioField,\
+    BooleanField, SubmitField, IntegerField, FormField, validators
+from wtforms.validators import Required
+from get_digits import get_digits
+
+class ExampleForm(Form):
+    steamid = IntegerField('Enter Steamid of User in Question.', description='This is the steamid of user to be considered.')
+
+    submit_button = SubmitField('Submit Form')
+
+
+def create_my_app(configfile=None):
+    my_app = Flask(__name__)
+    AppConfig(my_app, configfile)  # Flask-Appconfig is not necessary, but
+                                # highly recommend =)
+                                # https://github.com/mbr/flask-appconfig
+    Bootstrap(my_app)
+
+    # in a real app, these should be configured through Flask-Appconfig
+    my_app.config['SECRET_KEY'] = 'devkey'
+    my_app.config['RECAPTCHA_PUBLIC_KEY'] = \
+        '6Lfol9cSAAAAADAkodaYl9wvQCwBMr3qGR_PPHcw'
+
+    @my_app.route('/', methods=('GET', 'POST'))
+    def index():
+        form = ExampleForm()
+        form.validate_on_submit()  # to get error messages to the browser
+        flash('critical message', 'critical')
+        flash('error message', 'error')
+        flash('warning message', 'warning')
+        flash('info message', 'info')
+        flash('debug message', 'debug')
+        flash('different message', 'different')
+        flash('uncategorized message')
+        letters = []
+        if request.method == "POST":
+            #get steamid that the user has entered
+            steamid_to_process = request.form['steamid']
+            letters = get_digits(steamid_to_process)
+
+        return render_template('index.html', form=form, letters=letters)
+
+    return my_app
 
 if __name__ == '__main__':
-    app.run()
-
+    create_my_app().run(debug=True)
---
+++ get_digits.py
@@ -0,0 +1,4 @@
+def get_digits(n):
+    letters = [int(d) for d in str(n)]
+    return letters
+
